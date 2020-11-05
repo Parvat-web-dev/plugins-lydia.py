413176cd614b5cda301630defdc06db0ef71fef7
Rohithaditya <70464104+rohithaditya@users.noreply.github.com>
2020-11-05 10:42:23 +0530

    Create heroku_config.py

---
+++ heroku_config.py
@@ -0,0 +1,16 @@
+import os
+
+class Var(object):
+    APP_ID = int(os.environ.get("APP_ID", 6))
+    # 6 is a placeholder
+    API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    SUDO_USERS = os.environ.get("SUDO_USERS", "1207066133")
+    LYDIA_API_KEY = os.environ.get("LYDIA_API_KEY", None)
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+    TG_BOT_TOKEN_BF_HER = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
+    TG_BOT_USER_NAME_BF_HER = os.environ.get("TG_BOT_USER_NAME_BF_HER", None)
+    
+class Development(Var):
+    LOGGER = True
+    # Here for later purposes
